852dd0bc26ff49e879532b5f8bb2354a424e52d9
Dan Lopuch <dan@datahero.com>
2017-06-18 15:06:26 -0700

    Got gradle config figured out

--- src/test/java/AppGuiTest.java
+++ src/test/java/AppGuiTest.java
@@ -4,7 +4,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class AppTest {
+public class AppGuiTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
