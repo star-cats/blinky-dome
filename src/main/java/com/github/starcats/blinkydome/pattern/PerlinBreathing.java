2ad7f8c4944c9f235f49eb325c4be08a8212a840
Dan Lopuch <dlopuch@alum.mit.edu>
2017-07-09 18:32:19 -0700

    Cleaning up a bit

--- src/main/java/com/github/starcats/blinkydome/pattern/PerlinBreathing.java
+++ src/main/java/com/github/starcats/blinkydome/pattern/PerlinBreathing.java
@@ -23,16 +23,29 @@ public class PerlinBreathing extends LXPattern {
 
   public final CompoundParameter breathingPeriod;
   public final LXPerlinNoiseExplorer perlinNoiseField;
+  public final CompoundParameter speedModulationAmount;
 
-  private final VariableLFO position;
+  private final VariableLFO positionLFO;
 
-  private final FunctionalModulator speed;
+  private final SpeedModulator speedLFO;
   private final List<? extends LXPoint> points;
   private final ColorMappingSourceColorizer colorizer;
 
   private final LXVector up = new LXVector(0, 1, 0);
   private final LXVector down = new LXVector(0, -1, 0);
 
+
+  private abstract class SpeedModulator extends FunctionalModulator {
+    public SpeedModulator(String label, double start, double end, LXParameter periodMs) {
+      super(label, start, end, periodMs);
+    }
+
+    /**
+     * compute() must always return a normalized value 0-1, but this should return true when the current basis mapping
+     * is to be interpreted as a negative speedLFO.
+     */
+    abstract public boolean isNegative();
+  }
 
 
   public PerlinBreathing(LX lx, PApplet p, List<? extends LXPoint> points, ColorMappingSourceClan colorSource) {
@@ -46,49 +59,64 @@ public class PerlinBreathing extends LXPattern {
         .setUnits(LXParameter.Units.MILLISECONDS);
     addParameter(breathingPeriod);
 
-    position = new VariableLFO("pos");
-    position.waveshape.setValue(VariableLFO.Waveshape.SIN);
-    position.setPeriod(breathingPeriod);
-    addModulator(position);
-    position.start();
+    positionLFO = new VariableLFO("pos");
+    positionLFO.waveshape.setValue(VariableLFO.Waveshape.SIN);
+    positionLFO.setPeriod(breathingPeriod);
+    addModulator(positionLFO);
+    positionLFO.start();
 
-//    speed = new VariableLFO("speed");
-//    speed.waveshape.setValue(VariableLFO.Waveshape.SIN);
-//    speed.setPeriod(breathingPeriod);
-//    speed.phase.setValue(0.25); // This shift makes it a cosine, aka derivative of sin.  Speed is derivative of pos
-//    addModulator(speed);
-//    speed.start();
-
-    FunctionalModulator absSpeed = new FunctionalModulator("abs speed", 0., 1., breathingPeriod) {
+    speedLFO = new SpeedModulator("abs speedLFO", 0., 1., breathingPeriod) {
       @Override
       public double compute(double basis) {
+        // Speed must always be positive, so we get a normalized compute() response by just abs-valueing cosine
         double cos = Math.cos(2 * Math.PI * basis);
         return Math.abs(cos);
       }
+
+      @Override
+      public boolean isNegative() {
+        // Return true when cosine is negative
+        return getBasis() > 0.25 && getBasis() < 0.75;
+      }
     };
-    addModulator(absSpeed);
-    absSpeed.start();
-    speed = absSpeed;
+    addModulator(speedLFO);
+    speedLFO.start();
 
 
     perlinNoiseField = new LXPerlinNoiseExplorer(p, points, "br", "breathing");
     perlinNoiseField.setTravelVector(up);
-    addParameter(perlinNoiseField.noiseSpeed);
 
     this.colorizer = new ColorMappingSourceColorizer(perlinNoiseField, colorSource);
 
-    LXCompoundModulation speedModulation = new LXCompoundModulation(absSpeed, perlinNoiseField.noiseSpeed);
-    speedModulation.range.setValue(0.2);
+
+    LXCompoundModulation speedModulation = new LXCompoundModulation(speedLFO, perlinNoiseField.noiseSpeed);
+
+
+    // TODO: This is only needed b/c P3LX doesn't seem to show the built-in modulation device UI.  Remove if that bug
+    // fixed ??
+    speedModulationAmount = new CompoundParameter("speedLFO mod");
+    speedModulationAmount.setDescription("Amount of modulation the speedLFO modulator applies to perlin noise speed");
+    addParameter(speedModulationAmount);
+    speedModulationAmount.addListener(parameter -> {
+      speedModulation.range.setValue(parameter.getValue());
+    });
+    speedModulationAmount.setValue(0.2);
+
+
+    // The perlin field's speedLFO should always stay at 0.  Idea is the speedLFO modulates the speed parameter,
+    // moving it up and down with the up and down motion. The base value should stay at 0.
+    perlinNoiseField.noiseSpeed.setValue(0);
+    addParameter(perlinNoiseField.noiseSpeed); // adding it to see value of modulation, but not needed.
 
   }
 
   @Override
   protected void run(double deltaMs) {
-    //if (speed.getValue() > 0.5) {
-    if ( Math.cos(2 * Math.PI * speed.getBasis()) > 0 ) {
-      perlinNoiseField.setTravelVector(up);
-    } else {
+    //if (speedLFO.getValue() > 0.5) {
+    if ( speedLFO.isNegative() ) {
       perlinNoiseField.setTravelVector(down);
+    } else {
+      perlinNoiseField.setTravelVector(up);
     }
 
 
