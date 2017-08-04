ab65c36282222c259f94359ba6d91b3b119cda7e
Dan Lopuch <dlopuch@alum.mit.edu>
2017-08-03 17:54:39 -0700

    Updating fixture selector pattern to be able to select triangle sides

--- src/main/java/com/github/starcats/blinkydome/pattern/blinky_dome/BlinkyDomeFixtureSelectorPattern.java
+++ src/main/java/com/github/starcats/blinkydome/pattern/blinky_dome/BlinkyDomeFixtureSelectorPattern.java
@@ -11,20 +11,37 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- * Created by dlopuch on 6/28/17.
+ * Debugging pattern to select specific triangles (or parts of triangles) in blinky-dome
  */
 public class BlinkyDomeFixtureSelectorPattern
     extends AbstractFixtureSelectorPattern<BlinkyDome, BlinkyDomeFixtureSelectorPattern.BlinkyDomeFixtureType>
 {
+  public final EnumParameter<BlinkyDomeSubfixtureType> subfixtureParam;
+
   public enum BlinkyDomeFixtureType {
     GROUP,
-    TRIANGLE
+    TRIANGLE,
+    ALL
+  }
+
+  /** Which leg of the triangle to use*/
+  public enum BlinkyDomeSubfixtureType {
+    ALL,
+    X,
+    Y,
+    Z
   }
 
   public BlinkyDomeFixtureSelectorPattern(LX lx, BlinkyDome model) {
     super(lx, model);
+
+    subfixtureParam = new EnumParameter<>("Triangle leg", BlinkyDomeSubfixtureType.ALL)
+        .setDescription("Select which leg of the triangle to show for selected fixtures");
+    subfixtureParam.addListener(param -> updateFixtures());
+    addParameter(subfixtureParam);
   }
 
   @Override
@@ -42,6 +59,9 @@ public class BlinkyDomeFixtureSelectorPattern
       for (int i=0; i<model.allTriangles.size(); i++) {
         keys.add(i);
       }
+    } else if (fixtureFamily == BlinkyDomeFixtureType.ALL) {
+      return new Integer[] {0};
+
     } else {
       throw new RuntimeException("Unsupported fixture type: " + fixtureFamily);
     }
@@ -52,17 +72,36 @@ public class BlinkyDomeFixtureSelectorPattern
   @Override
   protected List<? extends LXFixture> getFixturesByKey(BlinkyDomeFixtureType fixtureFamily, Object keyObj) {
     Integer key = (Integer) keyObj;
+
+    List<BlinkyTriangle> fixtures;
+
     if (fixtureFamily == BlinkyDomeFixtureType.GROUP) {
-      return model.getTrianglesByGroup(key);
+      fixtures = model.getTrianglesByGroup(key);
 
     } else if (fixtureFamily == BlinkyDomeFixtureType.TRIANGLE) {
       BlinkyTriangle triangle = model.allTriangles.get(key);
       System.out.println("Selected triangle: " + triangle.toString());
-      return Collections.singletonList(triangle);
+      fixtures = Collections.singletonList(triangle);
+    } else if (fixtureFamily == BlinkyDomeFixtureType.ALL) {
+      fixtures = model.allTriangles;
 
     } else {
       throw new RuntimeException("Unsupported fixture type: " + fixtureFamily);
     }
+
+
+    // Subfixture filter:
+    if (subfixtureParam == null) {
+      return fixtures;
+    } else if (subfixtureParam.getEnum() == BlinkyDomeSubfixtureType.X) {
+      return fixtures.stream().map(f -> f.sX).collect(Collectors.toList());
+    } else if (subfixtureParam.getEnum() == BlinkyDomeSubfixtureType.Y) {
+      return fixtures.stream().map(f -> f.sY).collect(Collectors.toList());
+    } else if (subfixtureParam.getEnum() == BlinkyDomeSubfixtureType.Z) {
+      return fixtures.stream().map(f -> f.sZ).collect(Collectors.toList());
+    }
+
+    return fixtures;
   }
 
 
