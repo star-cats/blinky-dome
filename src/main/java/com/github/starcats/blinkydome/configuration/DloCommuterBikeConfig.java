0624124e3785af247b4f2b719eb862c353515b82
Dan Lopuch <dlopuch@alum.mit.edu>
2017-12-05 23:13:13 -0800

    Adding bike model

---
+++ src/main/java/com/github/starcats/blinkydome/configuration/DloCommuterBikeConfig.java
@@ -0,0 +1,106 @@
+package com.github.starcats.blinkydome.configuration;
+
+import com.github.starcats.blinkydome.color.GenericColorMappingSourceClan;
+import com.github.starcats.blinkydome.model.Icosastar;
+import com.github.starcats.blinkydome.model.dlo.DloCommuterBikeModel;
+import com.github.starcats.blinkydome.pattern.FixtureColorBarsPattern;
+import com.github.starcats.blinkydome.pattern.PalettePainterPattern;
+import com.github.starcats.blinkydome.pattern.PerlinBreathing;
+import com.github.starcats.blinkydome.pattern.PerlinNoisePattern;
+import com.github.starcats.blinkydome.pattern.RainbowZPattern;
+import com.github.starcats.blinkydome.pattern.mask.Mask_WipePattern;
+import com.github.starcats.blinkydome.util.StarCatFFT;
+import heronarts.lx.LX;
+import heronarts.lx.LXChannel;
+import heronarts.lx.LXPattern;
+import heronarts.lx.audio.BandGate;
+import heronarts.lx.modulator.LXModulator;
+import heronarts.lx.output.FadecandyOutput;
+import heronarts.lx.output.LXOutput;
+import heronarts.lx.transform.LXVector;
+import processing.core.PApplet;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Standard config for {@link Icosastar} model
+ */
+public class DloCommuterBikeConfig extends AbstractStarcatsLxConfig<DloCommuterBikeModel> {
+
+  // Components
+  private StarCatFFT starCatFFT;
+  protected GenericColorMappingSourceClan colorSampler;
+
+  // Modulators
+  private BandGate kickModulator;
+
+  public DloCommuterBikeConfig(PApplet p) {
+    super(p);
+  }
+
+  @Override
+  protected DloCommuterBikeModel makeModel() {
+    return DloCommuterBikeModel.makeModel(false);
+  }
+
+  @Override
+  protected List<LXOutput> constructOutputs(LX lx) {
+    return Arrays.asList(
+        new FadecandyOutput(lx, "localhost", 7890)
+    );
+  }
+
+  @Override
+  protected void initComponents(PApplet p, LX lx, DloCommuterBikeModel model) {
+    starCatFFT = CommonScLxConfigUtils.Components.makeStarcatFft(lx);
+    colorSampler = CommonScLxConfigUtils.Components.makeColorSampler(p, lx, starCatFFT);
+  }
+
+  @Override
+  protected List<LXModulator> constructModulators(PApplet p, LX lx, DloCommuterBikeModel model) {
+    kickModulator = CommonScLxConfigUtils.Modulators.makeKickModulator(lx);
+
+    return Arrays.asList(
+        kickModulator
+    );
+  }
+
+  @Override
+  protected void configChannel(int channelNum, LXChannel channel) {
+
+    // FixtureColorBarsPattern: Wire it up to engine-wide modulation sources
+    // --------------------
+    FixtureColorBarsPattern fixtureColorBarsPattern = new FixtureColorBarsPattern(lx, model.fixtures, colorSampler)
+        .initModulations(kickModulator);
+
+    // PerlinNoisePattern: apply defaults appropriate for Icosastar mapping size
+    // --------------------
+    PerlinNoisePattern perlinNoisePattern = new PerlinNoisePattern(lx, p, starCatFFT.beat, colorSampler);
+
+
+    // PerlinNoisePattern: apply defaults appropriate for BlinkyDomeModel mapping size
+    // --------------------
+    PerlinBreathing perlinBreathing = new PerlinBreathing(lx, p, model.getPoints(), colorSampler,
+        new LXVector(0, 0, -1),
+        new LXVector(0, 0, 1),
+        PerlinBreathing.BreathEasingSupplier.EXP_OUT_CUBIC_INOUT
+    ).initModulators();
+    perlinBreathing.perlinNoiseFieldZoom.setValue(0.02);
+    perlinBreathing.getSpeedModulationRange().setValue(0.20);
+
+
+    channel.setPatterns(new LXPattern[] {
+        perlinBreathing,
+        perlinNoisePattern,
+        // TODO: bring in FixtureTracerPattern
+        new RainbowZPattern(lx),
+        new PalettePainterPattern(lx, lx.palette), // feed it LX default palette (controlled by Studio's palette UI)
+        new Mask_WipePattern(lx),
+        fixtureColorBarsPattern
+    });
+
+    // TODO: config against headless raspi
+    // RaspiPerlinNoiseDefaults.applyPresetsIfRaspiGpio(perlinNoise);
+  }
+}
---
+++ src/main/java/com/github/starcats/blinkydome/model/dlo/DloCommuterBikeModel.java
@@ -0,0 +1,107 @@
+package com.github.starcats.blinkydome.model.dlo;
+
+
+import com.github.starcats.blinkydome.model.util.VectorStripModel;
+import heronarts.lx.model.LXFixture;
+import heronarts.lx.model.LXModel;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.transform.LXVector;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DloCommuterBikeModel extends LXModel {
+
+  private static final int NUM_LEDS_PER_STRIP = 64; // fadecandy port
+
+  public final List<LXFixture> framePieces;
+  public final LXFixture brakeBlob;
+
+  // Coordinate system scale factor to get approximately in sync with other models
+  private static final float ALPHA = 10;
+
+  // Bike geometry
+  private static final LXVector seatpostStripA = new LXVector(  6,    9, 0).mult(ALPHA);
+  private static final LXVector seatpostStripB = new LXVector(  7,    9, 0).mult(ALPHA);
+  private static final LXVector brakeBlobC     = new LXVector(  8,    8, 0).mult(ALPHA);
+  private static final LXVector handlebars     = new LXVector(-17,    9, 0).mult(ALPHA);
+  private static final LXVector stripsJoin     = new LXVector( -7, 1.5f, 0).mult(ALPHA);
+  private static final LXVector pedals         = new LXVector(  2,   -6, 0).mult(ALPHA);
+
+  private static final float BRAKE_BLOB_R = 1 * ALPHA;
+
+  // strip 0
+  private static final int BRAKE_BLOB_NUM_LEDS = 19;
+  private static final int LEN_SEATPOST_PEDALS = 46 - BRAKE_BLOB_NUM_LEDS;
+  private static final int LEN_PEDALS_HANDLEBARS_HALF = NUM_LEDS_PER_STRIP - LEN_SEATPOST_PEDALS - BRAKE_BLOB_NUM_LEDS;
+
+  // strip 1
+  private static final int LEN_SEATPOST_HANDLEBARS = 40;
+  private static final int LEN_HANDLEBARS_PEDALS_HALF = NUM_LEDS_PER_STRIP - LEN_SEATPOST_HANDLEBARS;
+
+  public static DloCommuterBikeModel makeModel(boolean hasGui) {
+    // If not gui, then Bike is running on raspi.  Get GPIO inputs.
+//    if (!hasGui) {
+//      RaspiGpio.init(outputProvider);
+//    }
+
+    List<LXFixture> allFixtures = new ArrayList<>();
+    List<LXFixture> framePieces = new ArrayList<>();
+
+    // Create the brake blob.  Model as a circle in the Z-axis
+    List<LXPoint> brakeBlobPoints = new ArrayList<>(BRAKE_BLOB_NUM_LEDS);
+    float thetaInc = (float)Math.PI * 2 / BRAKE_BLOB_NUM_LEDS;
+    LXVector brakeBlobRadial = new LXVector(0, BRAKE_BLOB_R, 0); // will rotate in z axis
+    for (int i=0; i<BRAKE_BLOB_NUM_LEDS; i++) {
+      LXVector brakeBlobLed = brakeBlobC.copy().add(brakeBlobRadial);
+      brakeBlobPoints.add(new LXPoint(brakeBlobLed.x, brakeBlobLed.y, brakeBlobLed.z));
+      brakeBlobRadial.rotate(thetaInc, 1, 0, 0);
+    }
+    LXFixture brakeBlob = () -> brakeBlobPoints; // new fixtures just define a getPoints() method.  Lambda that fucker.
+    allFixtures.add(brakeBlob);
+
+
+    LXFixture fixture = new VectorStripModel<>(seatpostStripB, pedals, VectorStripModel.GENERIC_POINT_FACTORY, LEN_SEATPOST_PEDALS);
+    allFixtures.add(fixture);
+    framePieces.add(fixture);
+
+    fixture = new VectorStripModel<>(pedals, stripsJoin, VectorStripModel.GENERIC_POINT_FACTORY, LEN_PEDALS_HANDLEBARS_HALF);
+    allFixtures.add(fixture);
+    List<LXPoint> pedalsToHandlebarsHalf = fixture.getPoints().stream().collect(Collectors.toList());
+    // Don't add to framePieces because we'll join this into one piece below
+
+
+    fixture = new VectorStripModel<>(seatpostStripA, handlebars, VectorStripModel.GENERIC_POINT_FACTORY, LEN_SEATPOST_HANDLEBARS);
+    allFixtures.add(fixture);
+    framePieces.add(fixture);
+
+    fixture = new VectorStripModel<>(handlebars, stripsJoin, VectorStripModel.GENERIC_POINT_FACTORY, LEN_HANDLEBARS_PEDALS_HALF);
+    allFixtures.add(fixture);
+    List<LXPoint> handlebarsToPedalHalf = fixture.getPoints().stream().collect(Collectors.toList());
+    // Don't add to framePieces because we'll join this into one piece below
+
+
+    // Join the two pedals<-->handlebars halves into one fixture
+    List<LXPoint> handlesbarsToPedalsSecondHalf = new ArrayList<>(pedalsToHandlebarsHalf);
+    Collections.reverse(handlesbarsToPedalsSecondHalf);
+    List<LXPoint> handlebarsToPedals = Stream.concat(
+        handlebarsToPedalHalf.stream(),
+        handlesbarsToPedalsSecondHalf.stream()
+    ).collect(Collectors.toList());
+    framePieces.add(() -> handlebarsToPedals); // lambda-ify new LXFixture()
+
+    return new DloCommuterBikeModel(allFixtures, framePieces, brakeBlob, hasGui);
+  }
+
+  private DloCommuterBikeModel(List<LXFixture> allFixtures, List<LXFixture> framePieces, LXFixture brakeBlob, boolean hasGui) {
+    super(allFixtures.toArray(new LXFixture[allFixtures.size()]));
+
+    System.out.println("Starting up with DloCommuterBikeModel model");
+
+    this.framePieces = framePieces;
+    this.brakeBlob = brakeBlob;
+  }
+}
